ff83696c956020e2044384c720c9a689c87939c6
Rob Cook <rpcook@cantab.net>
2025-08-08 17:37:46 +0100

    dates converted to integers and ocmbined into dictionary

--- webparser.py
+++ webparser.py
@@ -1,5 +1,5 @@
 import re
-import datetime
+from datetime import datetime
 
 def find_info(text):
     # find headings
@@ -16,14 +16,24 @@ def validate_info(info):
     return True
 
 def process_info(info):
-    # process date strings into integer days from XXXX
+    # process date strings into integer days from dd/mm/yyyy
     # return as dictionary of bin colours and date integers
-    pass
+    bin_collections = {
+        info[0][1]: int(datetime.strptime(info[1][1],'%d/%m/%Y').timestamp()/86400),
+        info[0][3]: int(datetime.strptime(info[1][4],'%d/%m/%Y').timestamp()/86400),
+        info[0][5]: int(datetime.strptime(info[1][7],'%d/%m/%Y').timestamp()/86400),
+        info[0][7]: int(datetime.strptime(info[1][10],'%d/%m/%Y').timestamp()/86400),
+        info[0][9]: int(datetime.strptime(info[1][13],'%d/%m/%Y').timestamp()/86400)
+    }
+    return bin_collections
 
 # Main execution
 if __name__ == '__main__':
     with open('scraped_source.htm') as f:
         source = f.read()
     source_info = find_info(source)
-    
-    print(source_info[0][0])
+    if not validate_info(source_info):
+        print('Scraped data not valid, quitting')
+        quit()
+    print(process_info(source_info))
+    # print(source_info)
