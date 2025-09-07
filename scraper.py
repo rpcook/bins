60f6b75bd087abdd7f617708ae0e2c6e9916614f
Rob Cook <rpcook@cantab.net>
2025-09-07 23:58:31 +0100

    form submission working, need to clean up the address look-up then fetch the proper results page

--- scraper.py
+++ scraper.py
@@ -2,17 +2,16 @@ import re
 # from datetime import datetime
 
 import requests
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
 def scrape_bin_date_website(street_address=None):
     session = requests.Session()
 
     # Get page (for cookie + webpage_token)
-    url = "https://waste.nc.north-herts.gov.uk/w/webpage/find-bin-collection-day-input-address"
-    r0 = session.get(url)
-    webpage_token = re.search(r"webpage_token=([a-f0-9]+)", r0.text).group(1)
-
-    # print("webpage_token:", webpage_token)
+    url_stem = "https://waste.nc.north-herts.gov.uk"
+    input_url = "/w/webpage/find-bin-collection-day-input-address"
+    r0 = session.get(url_stem+input_url)
+    # webpage_token = re.search(r"webpage_token=([a-f0-9]+)", r0.text).group(1)
     
     # Bootstrap POST (for form_check_ajax)
     headers = {"X-Requested-With": "XMLHttpRequest"}
@@ -21,35 +20,32 @@ def scrape_bin_date_website(street_address=None):
             "_session_storage": '{"_global":{"destination_stack":["w/webpage/find-bin-collection-day-input-address"]}}',
             "_update_page_content_request": "1"
         }
-    r1 = session.post(url, data=payload, headers=headers)
+    r1 = session.post(url_stem+input_url, data=payload, headers=headers)
 
-    dumpSource("r1.html", r1.json()["data"])
     # Extract CSRF from the XHR response
-    CSRF = re.search(r"CSRF = \'([a-fA-F0-9]+)", r1.text).group(1)
-    # print("CSRF:", CSRF)
-
-    # Request address input form
-    # headers = {
-    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
-    #     "Accept": "application/json, text/javascript, */*; q=0.01",
-    #     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    #     "X-Requested-With": "XMLHttpRequest",
-    #     "Origin": "https://waste.nc.north-herts.gov.uk",
-    #     "Referer": "https://waste.nc.north-herts.gov.uk/w/webpage/find-bin-collection-day-input-address",
-    # }
-    # payload = {
-    #     "_dummy": "1",
-    #     "_session_storage": '{"_global":{"destination_stack":["w/webpage/find-bin-collection-day-show-details?webpage_token=' + webpage_token + '","w/webpage/find-bin-collection-day-input-address"]}}',
-    #     "_update_page_content_request": "1",
-    #     "form_check_ajax": CSRF,
-    # }
-
-    # r2 = session.post(url, data=payload, headers=headers)
-    # form_source = r2.json()["data"]
+    # CSRF = re.search(r"CSRF = \'([a-fA-F0-9]+)", r1.text).group(1)
 
     # extract form input fields
-    # populate with street_address
-    # submit as form (POST?)
+    soup = BeautifulSoup(r1.json()["data"], "html.parser")
+    form_data = {}
+    for inp in soup.find_all("input"):
+        if inp.get("name"):  # only keep inputs with a name
+            form_data[inp["name"]] = inp.get("value", "")
+
+    # update fields with target address
+    # TODO: duplicate the address look-up autocomplete field (street_address is currently an integer ID field)
+    form_data[list(form_data.keys())[-2]] = street_address
+
+    # submit form with new payload
+    form_attr = soup.find("form").attrs
+    submission_url = form_attr["data-submit_destination"]
+
+    r2 = session.post(url_stem+submission_url, data=form_data, headers=headers)
+
+    redirect_url = r2.json()["redirect_url"]
+
+    # follow redirect
+    r3 = session.post(url_stem+redirect_url, headers=headers)
 
 def dumpSource(fname, source):
     with open(fname, "w", encoding="UTF-8") as f:
