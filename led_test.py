8cdb5680597742812a2aaf904288c217be358ff3
Rob Cook <rpcook@cantab.net>
2025-09-17 13:55:11 +0100

    updated led test complete

--- led_test.py
+++ led_test.py
@@ -4,12 +4,27 @@ import time
 GPIO.setmode(GPIO.BCM)
 # GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(9, GPIO.OUT)
+BUTTON_PIN = 5  # example pin (BCM numbering)
+BIN_BLUE = 17
+STATUS_GREEN = 18
 
-for i in range(5):
-    GPIO.output(9, GPIO.HIGH)
-    time.sleep(0.5)
-    GPIO.output(9, GPIO.LOW)
-    time.sleep(0.5)
+print("Waiting for touch-button press to toggle bin BLUE channel. CTRL+C to exit.")
 
-GPIO.cleanup()
+def button_pressed(channel):
+    # print("Button released!")
+    GPIO.output(BIN_BLUE, not GPIO.input(BIN_BLUE))
+
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BIN_BLUE, GPIO.OUT)
+GPIO.setup(STATUS_GREEN, GPIO.OUT)
+
+# Set up event detection (both edges or just falling/rising)
+GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=button_pressed, bouncetime=200)
+# GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=button_released, bouncetime=200)
+
+try:
+    while True:
+        time.sleep(0.5)  # keep the program alive
+        GPIO.output(STATUS_GREEN, not GPIO.input(STATUS_GREEN))
+except KeyboardInterrupt:
+    GPIO.cleanup()
